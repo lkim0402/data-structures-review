aadc631a16cbbaae89bf2fad10ea18d20e90806f
Leejun <estherleejunkim@gmail.com>
2025-11-16 20:05:41 +0900

    add BST

---
+++ src/datastructures/tree/BinarySearchTree.java
@@ -0,0 +1,18 @@
+package datastructures.tree;
+
+public class BinarySearchTree {
+
+  class Node {
+    int value;
+    Node left;
+    Node right;
+
+    Node(int value) {
+      this.value = value;
+    }
+  }
+
+  private Node root; // initialized as null
+
+
+}
---
+++ src/datastructures/tree/Main.java
@@ -0,0 +1,10 @@
+package datastructures.tree;
+
+public class Main {
+
+  public static void main(String[] args) {
+    BinarySearchTree myBST = new BinarySearchTree();
+    System.out.println(myBST.root);
+  }
+
+}
