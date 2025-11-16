0e27e059263f980764ce6c1d738629cf767ba3f8
Leejun <estherleejunkim@gmail.com>
2025-11-16 20:55:03 +0900

    add BST insert and contain

--- src/datastructures/tree/BinarySearchTree.java
+++ src/datastructures/tree/BinarySearchTree.java
@@ -12,7 +12,54 @@ public class BinarySearchTree {
     }
   }
 
-  private Node root; // initialized as null
+  public Node root; // initialized as null
 
+  public boolean insert(int value) {
+    Node newNode = new Node(value);
+
+    if (root == null) {
+      root = newNode;
+      return true;
+    }
+
+    Node temp = root;
+    while (true) {
+      if (temp.value == newNode.value) return false;
+
+      if (newNode.value < temp.value) {
+        // null spot
+        if (temp.left == null) {
+          temp.left = newNode;
+          return true;
+        }
+        //not null
+        temp = temp.left;
+      } else {
+        // null spot
+        if (temp.right == null) {
+          temp.right = newNode;
+          return true;
+        }
+        //not null
+        temp = temp.right;
+      }
+    }
+  }
+
+  public boolean contains(int val) {
+    Node temp = root;
+
+    while (true) {
+      if (val == temp.value) return true;
+
+      if (val < temp.value) {
+        if (temp.left == null) return false;
+        temp = temp.left;
+      } else {
+        if (temp.right == null) return false;
+        temp = temp.right;
+      }
+    }
+  }
 
 }
--- src/datastructures/tree/Main.java
+++ src/datastructures/tree/Main.java
@@ -4,7 +4,14 @@ public class Main {
 
   public static void main(String[] args) {
     BinarySearchTree myBST = new BinarySearchTree();
-    System.out.println(myBST.root);
+//    System.out.println(myBST.root);
+    myBST.insert(2);
+    myBST.insert(4);
+    myBST.insert(1);
+    System.out.println(myBST.contains(2));
+    System.out.println(myBST.contains(5));
+
+
   }
 
 }
